2b6411f9c659d9e37f79f40409ce3e0779763bc7
b2balexandre <seu-email@github.com>
2026-07-11 14:50:45 -0300

    Corrige duplicação de storage na URL de imagem de serviço

--- app/Models/Servico.php
+++ app/Models/Servico.php
@@ -35,6 +35,9 @@ class Servico extends Model
             return $value;
         }
 
+        $value = preg_replace('#^storage/#', '', $value);
+        $value = preg_replace('#^public/#', '', $value);
+
         return \Illuminate\Support\Facades\Storage::disk('public')->url($value);
     }
 
